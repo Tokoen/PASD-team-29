7196492bf2cacd1730fc9b4f40f79de552c65438
Tobian Koenen <tobiankoenen104@gmail.com>
2023-01-11 12:50:37 +0100

    database migration

--- dsd/delivery/models.py
+++ dsd/delivery/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 class Sender_Info(models.Model):
     name = models.CharField(max_length=50)
     street_and_number: models.CharField(max_length=50)
@@ -6,15 +7,34 @@ class Sender_Info(models.Model):
     city = models.CharField(max_length=85)
     country= models.CharField(max_length=56)
 
+class Receiver_Info(models.Model):
+    name = models.CharField(max_length=50)
+    street_and_number: models.CharField(max_length=50)
+    zipcode= models.CharField(max_length=8)
+    city = models.CharField(max_length=85)
+    country= models.CharField(max_length=56)
+
 class Order(models.Model):
-    question_text = models.CharField(max_length=200)
-    send_date = models.DateTimeField('send date')
-    x_in_mm = models.IntegerField(max_length=2000)
-    y_in_mm = models.IntegerField(max_length=2000)
-    z_in_mm = models.IntegerField(max_length=2000)
+    send_date = models.DateField('send date')
+    x_in_mm = models.IntegerField()
+    y_in_mm = models.IntegerField()
+    z_in_mm = models.IntegerField()
     is_breakable = models.BooleanField(default=False)
     is_perishable = models.BooleanField(default=False)
     sender_info = models.ForeignKey(Sender_Info, on_delete=models.CASCADE)
+    receiver_info = models.ForeignKey(Receiver_Info, on_delete=models.CASCADE)
+    id = models.IntegerField(unique=True, primary_key=True)
+
+class Delivery(models.Model):
+    expected_delivery_datetime = models.DateTimeField('expected delivery date')
+    actual_delivery_datetime = models.DateTimeField('actual delivery date')
+    order_id = models.IntegerField(unique=True)
+    cost_in_cents = models.IntegerField()
+    status = models.CharField(max_length=10)
+    id = models.IntegerField(unique=True, primary_key=True)
+
+
+
 
 
 
