729f9f3ca5628c1c07357c10c7f64e70e52a0172
Tobian Koenen <tobiankoenen104@gmail.com>
2023-01-15 17:49:42 +0100

    working login and search delivery

--- dsd/delivery/urls.py
+++ dsd/delivery/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('customer/', views.customer_page, name='customer_page'),
+    path('employee', views.employee_page, name='employee_page'),
+    path('search_delivery/', views.search_delivery, name='search_delivery'),
 ]
--- dsd/delivery/views.py
+++ dsd/delivery/views.py
@@ -1,5 +1,31 @@
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from .models import Delivery
 
+def is_customer(user):
+    return user.groups.filter(name='Customers').exists()
+
+def is_employee(user):
+    return user.groups.filter(name='Employees').exists()
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the deliveries index.")
+    if is_customer(request.user):
+        return HttpResponseRedirect("/delivery/customer")
+    elif is_employee(request.user):
+        return HttpResponseRedirect("/delivery/employee")
+    elif request.user.is_superuser:
+        return HttpResponseRedirect("/admin")
+
+def customer_page(request):
+    button_html = "<button>Click me</button>"
+    return render(request, 'delivery.html', {'button': button_html})
+
+def employee_page(request):
+    return HttpResponse("Employee page")
+
+def search_delivery(request):
+    delivery_id = request.POST.get('delivery_id')
+    delivery = get_object_or_404(Delivery, pk=delivery_id)
+    return render(request, 'delivery.html', {'delivery': delivery})
+
